9f0fd3b7a9d97a878b37e71c39a1e28247f98bbe
Hy <107110503+whanyu1212@users.noreply.github.com>
2024-04-23 04:26:17 +0000

    portfolio optimization function

--- src/port_optimizer.py
+++ src/port_optimizer.py
@@ -15,9 +15,9 @@ from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
 
 class PortfolioOptimizer:
     def __init__(self, price_df: pd.DataFrame, risk_free_rate: float = 0.0):
-        # Check that returns is a DataFrame
+        # Check that price_df is a DataFrame
         if not isinstance(price_df, pd.DataFrame):
-            raise ValueError("returns must be a DataFrame")
+            raise ValueError("price_df must be a DataFrame")
 
         # Check that risk_free_rate is a float
         if not isinstance(risk_free_rate, float):
@@ -31,15 +31,31 @@ class PortfolioOptimizer:
         S = risk_models.sample_cov(self.price_df)
         return mu, S
 
-    def maximize_portfolio_sharpe_ratio(self, expected_returns, cov_matrix):
+    def neg_sharpe_ratio(self, weights, expected_returns, cov_matrix, risk_free_rate):
+        portfolio_return = np.dot(weights, expected_returns)
+        portfolio_std_dev = np.sqrt(weights.T @ cov_matrix @ weights)
+        sharpe_ratio = (portfolio_return - risk_free_rate) / portfolio_std_dev
+        return -sharpe_ratio  # Minimizing negative Sharpe Ratio maximizes it
 
-        # Objective function: minimize the negative of the Sharpe Ratio
-        def neg_sharpe_ratio(weights):
-            portfolio_return = np.dot(weights, expected_returns)
-            portfolio_std_dev = np.sqrt(weights.T @ cov_matrix @ weights)
-            sharpe_ratio = (portfolio_return - self.risk_free_rate) / portfolio_std_dev
-            return -sharpe_ratio
+    def maximize_sharpe_ratio_by_mc(
+        self, expected_returns, cov_matrix, iterations=10000
+    ):
+        np.random.seed(42)
 
+        min_sharpe = 0
+
+        for i in range(iterations):
+            weights = np.random.random(len(self.price_df.columns))
+            weights /= np.sum(weights)
+            sharpe = self.neg_sharpe_ratio(
+                weights, expected_returns, cov_matrix, self.risk_free_rate
+            )
+            if sharpe < min_sharpe:
+                min_sharpe = sharpe
+                optimal_w = weights
+        return optimal_w
+
+    def maximize_sharpe_ratio_base(self, expected_returns, cov_matrix):
         # Initial guess for portfolio weights (equal distribution)
         initial_weights = np.full(len(expected_returns), 1 / len(expected_returns))
 
@@ -50,8 +66,9 @@ class PortfolioOptimizer:
         bounds = Bounds(0, 1)
 
         result = minimize(
-            neg_sharpe_ratio,
+            self.neg_sharpe_ratio,
             initial_weights,
+            args=(expected_returns, cov_matrix, self.risk_free_rate),
             method="trust-constr",
             bounds=bounds,
             constraints=constraints,
@@ -64,34 +81,8 @@ class PortfolioOptimizer:
             print("Optimization failed:", result.message)
             return None
 
-    def maximize_sharpe_ratio_with_sectors(
-        self, expected_returns, cov_matrix, risk_free_rate, sector_map, sector_bounds
-    ):
-        # sector_map = ['Financials', 'Financials', 'Financials', 'Telecommunications', 'Industrials', 'Industrials', 'Real Estate', 'Energy', 'Industrials', 'Real Estate', 'Consumer Discretionary']
-        # sector_bounds = {
-        #     "Financials": (0.2, 0.5),
-        #     "Real Estate": (0.1, 0.2),
-        #     "Telecommunications": (0.05, 0.15),
-        #     "Consumer Discretionary": (0.1, 0.2),
-        #     "Industrials": (0.1, 0.2),
-        # }
-        # Number of assets
-        n_assets = len(expected_returns)
-
-        # Objective function: minimize the negative of the Sharpe Ratio
-        def neg_sharpe_ratio(weights):
-            portfolio_return = np.dot(weights, expected_returns)
-            portfolio_std_dev = np.sqrt(weights.T @ cov_matrix @ weights)
-            sharpe_ratio = (portfolio_return - risk_free_rate) / portfolio_std_dev
-            return -sharpe_ratio  # Minimizing negative Sharpe Ratio maximizes it
-
-        # Initial guess for portfolio weights (equal distribution)
-        initial_weights = np.full(n_assets, 1 / n_assets)
-
-        # General constraint: Sum of weights equals 1
-        constraints = [LinearConstraint(np.ones(n_assets), lb=1, ub=1)]
-
-        # Sector constraints
+    def create_sector_constraints(self, sector_map, sector_bounds, n_assets):
+        constraints = []
         for sector, bounds in sector_bounds.items():
             sector_indices = [
                 i for i, asset in enumerate(sector_map) if asset == sector
@@ -100,18 +91,30 @@ class PortfolioOptimizer:
                 np.eye(n_assets)[sector_indices], lb=bounds[0], ub=bounds[1]
             )
             constraints.append(sector_constraint)
+        return constraints
 
-        # Bounds for weights, assuming no short selling and weights sum to 1
+    def maximize_sharpe_ratio_with_sectors(
+        self, expected_returns, cov_matrix, risk_free_rate, sector_map, sector_bounds
+    ):
+        n_assets = len(expected_returns)
+
+        initial_weights = np.full(n_assets, 1 / n_assets)
+
+        constraints = [LinearConstraint(np.ones(n_assets), lb=1, ub=1)]
+        constraints += self.create_sector_constraints(
+            sector_map, sector_bounds, n_assets
+        )
+
         bounds = Bounds(0, 1)
 
-        # Solve the optimization problem using the trust-constr method
         result = minimize(
-            neg_sharpe_ratio,
+            self.neg_sharpe_ratio,
             initial_weights,
+            args=(expected_returns, cov_matrix, risk_free_rate),
             method="trust-constr",
             bounds=bounds,
             constraints=constraints,
-            options={"verbose": 1},
+            options={"verbose": 0},
         )
 
         if result.success:
@@ -156,4 +159,15 @@ if __name__ == "__main__":
         mu, S, 0.0, sector_map, sector_bounds
     )
     for weight in optimized_weights:
+        print(weight)
+    print("\n")
+    print("Optimized weights without sectors:")
+    optimized_weights_without_sectors = po.maximize_portfolio_sharpe_ratio(mu, S)
+    for weight in optimized_weights_without_sectors:
+        print(weight)
+    print(type(optimized_weights_without_sectors))
+    print("\n")
+    print("Optimized weights by Monte Carlo:")
+    optimized_weights_by_mc = po.maximize_portfolio_sharpe_by_mc(mu, S)
+    for weight in optimized_weights_by_mc:
         print(weight)
